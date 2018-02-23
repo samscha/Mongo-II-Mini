da8810475ece68c041b0530fbce0a0278ae4686d
Samuel Cha <sunchipssam@gmail.com>
2018-02-23 07:57:12 -0800

    Added guided demo code

--- Authors/AuthorModel.js
+++ Authors/AuthorModel.js
@@ -5,13 +5,19 @@ const AuthorSchema = new mongoose.Schema({
     type: String,
     required: true,
     index: true,
+    // lowercase: true,
   },
   lastName: {
     type: String,
     required: true,
     index: true,
+    // lowercase: true,
   },
   createdOn: { type: Date, default: Date.now },
+});
+
+AuthorSchema.virtual('fullName').get(_ => {
+  return `${this.firstName} ${this.lastName}`;
 });
 
 module.exports = mongoose.model('Author', AuthorSchema);
--- server.js
+++ server.js
@@ -16,7 +16,36 @@ server.get('/', function(req, res) {
   res.status(200).json({ api: 'running' });
 });
 
-mongoose.connect('mongodb://localhost/library').then(
+// server.get('/api/books', (req, res) => {
+//   Book.find()
+//     .populate('authors', 'firstName lastName')
+//     .select('title') // book title
+//     .then(books => res.status(200).json(books))
+//     .catch(err => res.status(500).json(err));
+// });
+
+server.get('/api/books', (req, res) => {
+  Author.find()
+    // .select('firstName lastName fullName')
+    // .or([{ firstName: 'Pramod' }, { firstName: 'Martin' }])
+    // .limit(5)
+    // .sort('-firstName')
+    // .populate('authors', 'firstName lastName')
+    .then(authors => res.status(200).send(authors))
+    // .select('_id')
+    // .populate('books', 'title')
+    // .select('title') // book title
+    // .then(ids =>
+    //   Book.find()
+    //     .where('authors')
+    //     .in(ids)
+    //     .populate('authors', 'firstName lastName')
+    //     .then(books => res.status(200).send(books)),
+    // )
+    .catch(err => res.status(500).json(err));
+});
+
+mongoose.connect('mongodb://localhost/cs6').then(
   () => {
     const port = process.env.PORT || 3000;
     server.listen(port);
@@ -26,5 +55,21 @@ mongoose.connect('mongodb://localhost/library').then(
     console.log('\n************************');
     console.log("ERROR: Couldn't connect to MongoDB. Do you have it running?");
     console.log('************************\n');
-  }
+  },
 );
+
+// lowercase: true,
+// Schema( {
+
+// },
+// {
+//    runSettersOnQuery: true
+// }
+// )
+
+// virtuals in Model
+// AuthorSchema.virtual('fullName')
+//   .get(_ => {
+//     return `${this.firstName} ${this.lastName}`
+//    }) //retrieve
+//   .set() // save
